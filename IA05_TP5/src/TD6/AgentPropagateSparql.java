06a90450c990bce65e8ee3b5d0589ba9a3ae23c5
AudreyB <AudreyB@192.168.1.15>
2014-05-04 21:51:17 +0200

    Fix error AgentPropagateSparql

--- IA05_TP5/src/TD6/AgentPropagateSparql.java
+++ IA05_TP5/src/TD6/AgentPropagateSparql.java
@@ -10,16 +10,28 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+
 
 public class AgentPropagateSparql extends Agent {
 
 	private static final long serialVersionUID = 1L;
 	protected ObjectMapper mapper = new ObjectMapper();
 	
+	/**
+	 * Réception d'un message de la plateforme Jade 
+	 * Envoie du type de requête et du fichier pour initialiser le model.
+	 * @author AudreyB
+	 *
+	 */
 	private class ReceiveRequest extends Behaviour{
 
 		
@@ -37,8 +49,14 @@ public class AgentPropagateSparql extends Agent {
 					ir.setRequestType(msg.getReqType());
 					mapper.writeValue(sw, ir);
 					
-					message.addReceiver(r);
-					message.setConversationId(myAgent.getAID()+DateFormat);
+					message.addReceiver(getReceiver("Agent", "KB"));
+					message.setPerformative(ACLMessage.INFORM);
+					String cid = (myAgent.getAID()+""+message.getPostTimeStamp());
+					message.setConversationId(cid);
+					
+					addBehaviour(new FormatRequestBehav(cid));
+					
+					send(message);
 					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -59,7 +77,6 @@ public class AgentPropagateSparql extends Agent {
 	
 	/**
 	 * Receive message from Jade plateform
-	 * Create a conversationId 
 	 * send a first Message with a content InformRequest with the conversationId created
 	 * Create the Sparql Request with message information
 	 * send a second message with a request with the same  conversationId
@@ -67,10 +84,19 @@ public class AgentPropagateSparql extends Agent {
 	 *
 	 */
 	private class FormatRequestBehav extends Behaviour {
+		
+		private String conversId;
+		
+		public FormatRequestBehav(String cid){
+			this.conversId = cid;
+		}
 
 		@Override
 		public void action() {
-			// TODO Auto-generated method stub
+			/**
+			 * Préparer la requête (écriture dans un fichier type "query.sparql"
+			 * Envoie du nom de fichier à l'agent KB pour l'exécution de la requête
+			 */
 			
 		}
 
@@ -81,5 +107,21 @@ public class AgentPropagateSparql extends Agent {
 		}
 		
 	}
+	
+	private AID getReceiver(String type, String Name) {
+        AID rec = null;
+        DFAgentDescription template =new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(type);
+        sd.setName(Name);
+        template.addServices(sd);
+        try {
+            DFAgentDescription[] result = DFService.search(this, template);
+            rec = result[0].getName();
+        } catch(FIPAException fe) {
+            System.err.println(fe);
+        }
+        return rec;
+    }
 
 }
